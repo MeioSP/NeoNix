8fe23334de1a6fd9a4cbcf5e573115a9209ab9a4
sn49 <34567701+sn49@users.noreply.github.com>
2021-05-07 11:10:31 +0900

    login,register recoding

---
+++ TestGameSparks/Assets/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        string id = GameObject.Find("idinput").GetComponent<InputField>().text;
+        string psw = GameObject.Find("pswinput").GetComponent<InputField>().text;
+    }
+}
---
+++ TestGameSparks/Assets/Register.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class Register : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        string id = GameObject.Find("idinput").GetComponent<InputField>().text;
+        string psw = GameObject.Find("pswinput").GetComponent<InputField>().text;
+        string repsw = GameObject.Find("repswinput").GetComponent<InputField>().text;
+        string nickname = GameObject.Find("nicknameinput").GetComponent<InputField>().text;
+
+        if (psw==repsw)
+        {
+            var request = new RegisterPlayFabUserRequest { Email = id, Password = psw, DisplayName=nickname};
+            PlayFabClientAPI.RegisterPlayFabUser(request, RegisterSuccess, RegisterFailed);
+        }
+        
+    }
+
+    void RegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("가입 성공");
+    }
+
+    void RegisterFailed(PlayFabError error)
+    {
+        Debug.LogWarning("가입 실패");
+        Debug.LogWarning(error.GenerateErrorReport());
+    }
+}
